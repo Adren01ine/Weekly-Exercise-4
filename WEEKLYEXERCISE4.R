a3e2c200a6b0be19bbcbc8fdc7940eb5a9eff189
Adren01ine <sdai24@uic.edu>
2025-09-24 15:50:31 -0500

    dev

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,1 +1,9 @@
-test
+library(readr)
+library(tidyverse)
+
+Olympics <- readr::read_csv("Olympics.csv") %>% mutate(total.medals = gold + silver + bronze)
+
+country_gold <- Olympics %>% group_by(country) %>% summarise(total_gold = sum(gold, na.rm = TRUE))
+
+year_medals <- Olympics %>% group_by(year) %>% summarise(total_medals_given = sum(total.medals, na.rm = TRUE))
+
